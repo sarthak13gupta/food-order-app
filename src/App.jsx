80bd2554c48c4c63c32d5be6f6d1bae55face7dd
Sarthak Gupta <sarthak_g@ee.iitr.ac.in>
2021-12-18 13:49:17 +0530

    added meal-items

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 
+import Meals from "./Components/Meals/Meals";
 import Header from "./Components/Layout/Header";
 
 function App() {
   return (
     <Fragment>
       <Header/>
+      <main><Meals/></main>
     </Fragment>
       
     
