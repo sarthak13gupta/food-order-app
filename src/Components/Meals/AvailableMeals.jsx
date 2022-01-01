fe817266cfb1114b9c6c17968a09af11923b4f7c
Sarthak Gupta <sarthak_g@ee.iitr.ac.in>
2022-01-01 20:34:31 +0530

    add items to cart

--- src/Components/Meals/AvailableMeals.jsx
+++ src/Components/Meals/AvailableMeals.jsx
@@ -32,15 +32,13 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
-    <li>
-      <MealItem
-        id = {meal.id}
-        key={meal.id}
-        name={meal.name}
-        description={meal.description}
-        price={meal.price}
-      />
-    </li>
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
   ));
   return (
     <section className={classes.meals}>
